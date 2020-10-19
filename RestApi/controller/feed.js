3e45c06b7a518968296dc022cf4b9311100dd315
Narmin96 <nasirova@archis.academy>
2020-10-19 23:09:45 +0400

    Rest Api example

---
+++ RestApi/controller/feed.js
@@ -0,0 +1,20 @@
+exports.getPosts=(req,res,next)=>{
+    res.status(200).json({
+        posts:[{
+            fullname:'Narmin Nasirova',
+            department:'Software Development',
+            salary:700.00
+
+        }]
+    })
+}
+exports.createPost=(req,res,next)=>{
+    const full_name=req.body.fullname;
+    const department=req.body.department;
+    const salary=req.body.salary;
+    res.status(201).json({
+        message:"Post created succesfully!",
+        post:{id:new Date().toISOString(),fullname:full_name,department:department,salary:salary}
+    
+    });
+};
---
+++ RestApi/routes/feed.js
@@ -0,0 +1,9 @@
+const express=require('express');
+
+const feedController=require('../controller/feed');
+
+const router=express.Router();
+
+router.get('/posts',feedController.getPosts);
+router.post('/post',feedController.createPost);
+module.exports=router;
